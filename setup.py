9132bd9adcd66f6b9c52adc1459c0ac2bc58485d
glacials <qhiiyr@gmail.com>
2020-07-30 15:25:58 -0700

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yourbase",
-    version="0.0.3",
+    version="0.0.4",
     author="YourBase",
     author_email="ben@yourbase.io",
     description="Test acceleration usable in the YourBase CI",
